3be07cf68dea95299e2e6c1f51fc61f16ae8a112
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-10 14:07:45 -0500

    new added

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -4,6 +4,9 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening class!");
 		System.out.println("Hello World");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello Everybody ");
+		}
 	}
 
 }
